66d3c64ad1c6327006980af2e195d56740b3d40d
quq99 <eequq99@gmail.com>
2019-04-17 15:01:30 -0400

    pa4 complete

--- PA4/testpa4.sh
+++ PA4/testpa4.sh
@@ -27,6 +27,7 @@ echo "========================================="
 echo "diff all the .s file in TestPA4/WorkingTest"
 echo "       and in TestPA4/WorkingTest/expectOutput"
 echo "========================================="
+rm expectOutput/*.txt
 for filename in `ls expectOutput/`
 do
     echo "diff ${filename} with expected file"
@@ -34,14 +35,23 @@ do
     echo "if no other information, ${filename} OK"
 done
 
-#echo " "
-#echo "========================================="
-#echo "testing error cases in TestPA3/ErrorTest"
-#echo "========================================="
-#cd ../ErrorTest
-#for filename in `ls *.java`
-#do
-#    echo "++++++++++++++++++++"
-#    echo "test file ${filename}"
+echo " "
+echo "========================================="
+echo "testing MJSIM.jar output"
+echo "========================================="
+suffix=".java"
+for filename in `ls *.java`
+do
+    echo "++++++++++++++++++++"
+    echo "test file ${filename}"
+    javac ${filename}
+    file=${filename%$suffix}
+    java ${file} >& expectOutput/${file}.referout.txt
+
+    java -jar ../../MJSIM.jar -b -f ${filename}.s >& ./${file}.out.txt
+
+    echo "diff ${filename} with expected file"
+	diff expectOutput/${file}.referout.txt ./${file}.out.txt
+    echo "if no other information, ${filename} OK"
 #    java -jar ../../MJ.jar ${filename}
-#done
+done
